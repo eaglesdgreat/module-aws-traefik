5af91167a595281235c48e30015d7878b7acc171
Emmanuel <emmanuel.okponobi@zedcrest.com>
2025-10-23 23:28:09 +0100

    Module aws traefik

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "helm" {
+  kubernetes = {
+    config_path            = null
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "helm_release" "traefik-ingress" {
+  name       = "ms-traefik-ingress"
+  repository = "https://helm.traefik.io/traefik"
+  chart      = "traefik"
+  namespace  = "kube-system"
+
+  values = [<<EOF
+  service:
+    annotations:
+      service.beta.kubernetes.io/aws-load-balancer-type: nlb
+      externalTrafficPolicy: Local
+  EOF
+  ]
+}
