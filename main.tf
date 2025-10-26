a243dcefb35d33146aefa73336f4723dd5bce100
Emmanuel <emmanuel.okponobi@zedcrest.com>
2025-10-26 08:09:02 +0100

    Removed version added

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ resource "helm_release" "traefik-ingress" {
   name          = "ms-traefik-ingress"
   repository    = "https://helm.traefik.io/traefik"
   chart         = "traefik"
-  version       = "5.46.8"
   timeout       = 2400 # Increase to 20 minutes
   wait          = true
   wait_for_jobs = true
